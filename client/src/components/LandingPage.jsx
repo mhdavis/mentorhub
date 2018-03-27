7dfd4d328673ad2994325d83fb3c08be8af0475b
Michael Davis <mhdavis1993@gmail.com>
2018-03-27 16:01:03 -0400

    Added Semantic UI and started LandingPage

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Mentorhub!</h1>
+        <p>lend a hand</p>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
