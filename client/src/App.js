77077566011513f85e2756ef4f16dfb6f69e6e7a
Michael Davis <mhdavis1993@gmail.com>
2018-03-29 09:44:10 -0400

    Got about page route working and instantiated navbar component file

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import LandingPageLayout from "./components/LandingPage/LandingPageLayout";
+import AboutPageLayout from "./components/AboutPage/AboutPageLayout";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route exact path="/" component={LandingPageLayout} />
+            <Route exact path="/about" component={AboutPageLayout} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ client/src/components/AboutPage/AboutPageLayout.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Grid, Header, Image, Segment } from "semantic-ui-react";
+import ResponsiveContainer from "../ResponsiveContainer";
+
+const AboutPageLayout = () => (
+  <ResponsiveContainer>
+    {/* This should be location of feed */}
+    <Segment style={{ padding: "8em 0em" }} vertical>
+      <Grid container stackable verticalAlign="middle">
+        {/* Mentee Information */}
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as="h3" style={{ fontSize: "2.2em" }}>
+              Find some folks to learn from...
+            </Header>
+            <p style={{ fontSize: "1.6em" }}>This is the about page</p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            <Image
+              bordered
+              rounded
+              size="large"
+              src="/assets/img/white-image.png"
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+    {/* Mentor Information */}
+    <Segment style={{ padding: "8em 0em" }} vertical>
+      <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column floated="left" width={6}>
+            <Image
+              bordered
+              rounded
+              size="large"
+              src="/assets/img/white-image.png"
+            />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Header as="h3" style={{ fontSize: "2.2em" }}>
+              ...Or find some folks to teach!
+            </Header>
+            <p style={{ fontSize: "1.6em" }}>
+              Find Mentees or Groups of Mentees to help guide in their personal
+              and professional journey. Grow your teaching skills and network
+              along the way!
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+    {/* Community Information */}
+    <Segment style={{ padding: "8em 0em" }} vertical>
+      <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column
+            width={16}
+            textAlign="center"
+            style={{ padding: "1.6rem 10rem" }}
+          >
+            <Header as="h3" style={{ fontSize: "2.2em" }}>
+              All for free!
+            </Header>
+            <p style={{ fontSize: "1.6em" }}>
+              We strive to build a community where the only currency in the
+              bonds folks will build with eachother. Bonds with longterm and
+              lasting impacts!
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  </ResponsiveContainer>
+);
+
+export default AboutPageLayout;
